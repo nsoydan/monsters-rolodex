76a53f7677809ea02027d7296760318b67cf686e
nihat soydan <nsoydan@gmail.com>
2022-08-29 09:47:43 +0300

    added searc-box with removing character property successfully and save the some variables in state to use somewhere in app.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ class App extends Component {
   constructor (){
     super();
 
+    console.log("Constructor");
     this.state={
       monsters:[],
+      searchString:"",
     }
   }
 
   componentDidMount(){
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=>response.json())
       .then((users)=>
@@ -20,14 +23,39 @@ class App extends Component {
           ()=>{
             return {monsters:users}
           },
-          ()=>console.log(this.state))
-      )
+          ()=>console.log(this.state)
+      ))
   }
 
   render(){
+    console.log("render")
+
+    // Find filtered monsters before every render
+    const filteredMonsters= this.state.monsters.filter(
+      (monster)=>{
+        return monster.name.toLocaleLowerCase().includes(this.state.searchString)
+      });
+
+
     return (
       <div className="App">
-       {this.state.monsters.map((monster)=>{
+        <input 
+        className='search-box' 
+        type='search' 
+        placeholder='search monster'
+        onChange={(e)=>{
+          console.log(e)
+          const searchString=e.target.value.toLocaleLowerCase()
+               
+        this.setState(
+          ()=>{
+            return {searchString}     // Save searchString in state to use everywhere
+         })  
+        }} />
+
+       {/* Map filtered monsters to render */}
+
+       {filteredMonsters.map((monster)=>{
           return(
             <div key={monster.id}>
               <h1>{monster.name}</h1>
